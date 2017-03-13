d56807c05fa18ecb11cd6f876a8af3abb7403725
vasujain00 <www.vasujain@gmail.com>
2017-03-12 17:39:45 -0700

    changed

--- showproducts.php
+++ showproducts.php
@@ -1,40 +1,24 @@
 <?php 
 include ('inc/connect.php');
 
-$query=  mysql_query("SELECT * FROM product") or die(mysql_error());
-$num=  mysql_num_rows($query);
+$query=  mysqli_query($link,"SELECT * FROM product") or die(mysqli_error($link));
+$num=  mysqli_num_rows($query);
+
+
  if($num>0){
- 	$i=0;
- 	while($i<$num)
- 	{
- 		$name=mysql_result($query,$i,"pname");
-        $price=mysql_result($query,$i,"price");
-        $description=mysql_result($query,$i,"description");
-        $discount=mysql_result($query,$i,"dicount");
- 	}
-?>
+ 
+        while ($row = mysqli_fetch_array($query)) {
+
+            $ProName=$row['Pname'];
+             echo $ProName;
+        } 
+      }
 
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0,user-scalable=no">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<title>Home Decor</title>
-</head>
-<body>
-<div class="container">
-<div class="row">
-    <div class="col-sm-3" style="background-color:lavender;">.col-sm-4</div>
-    <div class="col-sm-3" style="background-color:lavenderblush;"></div>
-    <div class="col-sm-3" style="background-color:lavender;"></div>
-  </div>
-</div>
+        ?>
+      
 
 
 
-</body>
-</html>
+
+    
