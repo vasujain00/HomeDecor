30fc3d70feaf4d9bf5ae7065677353e3bb1fe8c7
Anurag131277 <anurag1887@gmail.com>
2017-03-19 02:55:24 +0530

    changes made in home--> -added slider -links added for seller login and customer login -added seperate sesseion for admin login and customer login and seller login

--- home.php
+++ home.php
@@ -1,4 +1,16 @@
-<?php  ?>
+<?php 
+ session_start();
+
+ if(isset($_SESSION['customeremail'])){
+ 	header('Location : customer.php');
+ 	exit();
+ }
+ else if(isset($_SESSION['selleremail'])){
+ 	header('Location : seller.php');
+ 	exit();
+ }
+
+ ?>
 
 <!doctype html>
 
@@ -11,8 +23,83 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
    <link rel="stylesheet" href="styles.css">
+   <link rel="stylesheet" href="css/first.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <title>Home Decor | Home</title>
+   <style>
+.slide-img{
+   width: 100%;
+  height: 470px;
+}
+.slider {
+  position: relative;
+  width: 100%;
+  height: 470px;
+  border-bottom: 1px solid #ddd;
+}
+.slide {
+  background: transparent  center center no-repeat;
+  background-size: cover;
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.active-slide {
+    display: block;
+}
+.slider-nav {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.arrow-prev {
+  margin-right: 45px;
+  display: inline-block;
+  vertical-align: top;
+  margin-top: 9px;
+}
+
+.arrow-next {
+  margin-left: 45px;
+  display: inline-block;
+  vertical-align: top;
+  margin-top: 9px;
+}
+
+.slider-dots {
+  list-style: none;
+  display: inline-block;
+  padding-left: 0;
+  margin-bottom: 0;
+}
+
+.slider-dots li {
+  color: #bbbcbc;
+  display: inline;
+  font-size: 30px;
+  margin-right: 5px;
+}
+
+.slider-dots li.active-dot {
+  color: #363636;
+}
+.navimg{
+	position: absolute;
+	margin-top: 180px;
+	z-index: 1;
+	
+}
+#left{
+float: left;
+}
+#right{
+	right: 0;
+	float:right;
+}
+</style>
 </head>
 <body>
 <div  class="heading">
@@ -47,7 +134,7 @@
       <ul class="sub-menu">
          <li><a href='login.php'><span>CUSTOMER LOGIN</span></a></li>
 		 <li><a href='login.php'><span>SELLER LOGIN</span></a></li>
-		 <li class='last'><a href='login.php'><span>ADMIN LOGIN</span></a></li>
+		 <li class='last'><a href='adminlogin.php'><span>ADMIN LOGIN</span></a></li>
 		 </ul>
 		 </li>
     
@@ -56,7 +143,56 @@
    <li class='last'><a href='contact.html'><span>Contact</span></a></li>
 </ul>
 </div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="slider">
+ 			<span class="navimg" id="left"><a  class="arrow-prev"><img src="image/sliders/slider-left.png"></a></span>
+            <span class="navimg" id="right"><a class="arrow-next"><img src="image/sliders/slider-right.png"></a></span>
 
+              <div class="slide active-slide">
+                <div class="container-fluid">
+                  <div class="row">
+                    <img class="slide-img" src="image/sliders/img1.jpg" >
+                  </div>
+                </div>
+              </div>
+              <div class="slide">
+                <div class="container-fluid">
+                  <div class="row">
+                     <img class="slide-img"src="image/sliders/img2.jpg" >
+                  </div>
+                </div>
+              </div>
+              <div class="slide">
+                <div class="container-fluid">
+                  <div class="row">
+                     <img class="slide-img" src="image/sliders/img3.jpg" >
+                  </div>
+                </div>
+              </div>
+
+              <div class="slide">
+                <div class="container-fluid">
+                  <div class="row">
+                     <img class="slide-img" src="image/sliders/img4.jpg" >
+                  </div>
+                </div>
+              </div>
+
+              <div class="slide">
+                <div class="container-fluid">
+                  <div class="row">
+                     <img class="slide-img" src="image/sliders/img5.jpg" >
+                  </div>
+                </div>
+              </div>
+              
+          </div>
+			
+		</div>
+	</div>
+</div>
 <div>
 <footer id="footer">
     <div class="footer-bottom">
@@ -68,5 +204,48 @@
     </div>
 </footer>
 </div>
+
+
+<script type="text/javascript">
+              var main=function(){
+          /* IMAGE SLIDER */
+            $('.arrow-next').click(function(){
+              var currentSlide=$('.active-slide');
+              var nextSlide=currentSlide.next();
+
+             
+              
+
+              if(nextSlide.length === 0){
+                nextSlide=$('.slide').first();
+                
+              }
+
+              currentSlide.fadeOut(600).removeClass('active-slide');
+              nextSlide.fadeIn(600).addClass('active-slide');
+
+              //currentDot.removeClass('active-dot');
+              //nextDot.addClass('active-dot');
+            });
+
+            $('.arrow-prev').click(function(){
+              var currentSlide =$('.active-slide');
+              var prevSlide=currentSlide.prev();
+
+              
+
+              if(prevSlide.length === 0){
+                prevSlide=$('.slide').last();
+              
+              }
+
+              currentSlide.fadeOut(600).removeClass('active-slide');
+              prevSlide.fadeIn(600).addClass('active-slide');
+
+            }); 
+          }
+          $(document).ready(main);
+
+          </script>
 </body>
 <html>
