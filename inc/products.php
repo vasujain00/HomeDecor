1b3a7f9753c6cc2757fe98a2e3c280b8e9c34e94
Anurag131277 <anurag1887@gmail.com>
2017-03-30 18:31:56 +0530

    product.php contains product class which encapsulates functions for bestprice and all_product printing

---
+++ inc/products.php
@@ -0,0 +1,83 @@
+<?php 
+include_once ('inc/connect.php');
+
+class Product{
+
+  public function show_product(){
+    $db=new Database();
+    $db->dbConnect();
+    $link=$db->getConn();
+    $query=  mysql_query("SELECT * FROM product") or die(mysql_error());
+    $num=  mysql_num_rows($query);
+     if($num>0){
+      $i=0;
+        //print_r(mysql_result($query,0));
+
+      while($i<$num)
+      {
+            $prod_id=mysql_result($query,$i,"Prod_ID");
+            $brand_id=mysql_result($query,$i,"Brand_ID");
+            $cart_id=mysql_result($query,$i,"Cart_ID");
+            $color=mysql_result($query,$i,"Color");
+            $name=mysql_result($query,$i,"pname");
+            $price=mysql_result($query,$i,"price");
+            $image=mysql_result($query, $i,"Image");
+            $description=mysql_result($query,$i,"Descri");
+            $discount=mysql_result($query,$i,"Discount");
+            $i=$i+1;
+            $image="products/".$image;
+           echo "
+                  <div class=\"w3-card-4 w3-cell\">
+                    <img class=\"img_size\" src=\"".$image."\" alt=\"Norway\">
+                    <div class=\"w3-container w3-center\">
+                      <h1>".$name."</h1>
+                      <p>".$color."<br>".$price."<br>".$description."</p>
+                    </div>
+                  </div>
+
+           ";
+
+      }
+    }
+  }
+
+  public function best_price(){
+    $db=new Database();
+    $db->dbConnect();
+    $query=mysql_query("SELECT * FROM `product` WHERE `Price`<=50") or die(mysql_error());
+    $num=mysql_num_rows($query);
+     if($num>0){
+      $i=0;
+        //print_r(mysql_result($query,0));
+
+      while($i<$num)
+      {
+            $prod_id=mysql_result($query,$i,"Prod_ID");
+            $brand_id=mysql_result($query,$i,"Brand_ID");
+            $cart_id=mysql_result($query,$i,"Cart_ID");
+            $color=mysql_result($query,$i,"Color");
+            $name=mysql_result($query,$i,"pname");
+            $price=mysql_result($query,$i,"price");
+            $image=mysql_result($query, $i,"Image");
+            $description=mysql_result($query,$i,"Descri");
+            $discount=mysql_result($query,$i,"Discount");
+            $i=$i+1;
+            $image="products/".$image;
+            
+            echo "
+
+                  <div class=\"w3-card-4 w3-cell\">
+                    <img class=\"img_size\" src=\"".$image."\" alt=\"Norway\">
+                    <div class=\"w3-container w3-center\">
+                      <h1>".$name."</h1>
+                      <p>".$color."<br>".$price."<br>".$description."</p>
+                    </div>
+                  </div>
+            ";
+
+      } //While closed
+  }  //If closed
+} //best_price() closed
+
+}//class Product closed
+?>
