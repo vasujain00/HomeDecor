8ce42463d45255ef9555539aeb4057442f3a28c7
Anurag131277 <anurag1887@gmail.com>
2017-04-16 23:46:23 +0530

    PRoduct template changed to an image template. a dummy product image is used locally, change the link refrence of the images to their original form

--- inc/products.php
+++ inc/products.php
@@ -3,11 +3,11 @@ include_once ('connect.php');
 
 class Product{
 
-  public function show_product(){
+  public function show_product($limit){
     $db=new Database();
     $db->dbConnect();
     $link=$db->getConn();
-    $query=  mysql_query("SELECT * FROM product") or die(mysql_error());
+    $query=  mysql_query("SELECT * FROM product LIMIT ".$limit) or die(mysql_error());
     $num=  mysql_num_rows($query);
      if($num>0){
       $i=0;
@@ -15,7 +15,7 @@ class Product{
 
       while($i<$num)
       {
-            $prod_id=mysql_result($query,$i,"Prod_ID");
+            $prod_id=mysql_result($query,$i,"ID");
             $brand_id=mysql_result($query,$i,"Brand_ID");
             $cart_id=mysql_result($query,$i,"Cart_ID");
             $color=mysql_result($query,$i,"Color");
@@ -27,9 +27,9 @@ class Product{
             $i=$i+1;
             $image="products/".$image;
            echo "
-              <div class=\"col-md-3\">
+              <div class=\"col-md-4\">
                  
-                   <img src=\"".$image."\" class=\"img-thumbnail\" alt=\"Cinque Terre\" width=\"304\" height=\"236\">  
+                   <img src=\"products/skull_clock.jpg\" class=\"img-thumbnail\" alt=\"Cinque Terre\" width=\"304\" height=\"236\">  
                     
                  </div>
                
@@ -68,10 +68,10 @@ class Product{
 
   }
 
-  public function best_price(){
+  public function best_price($limit){
     $db=new Database();
     $db->dbConnect();
-    $query=mysql_query("SELECT * FROM `product` WHERE `Price`<=50") or die(mysql_error());
+    $query=mysql_query("SELECT * FROM `product` WHERE `Price`<=50 LIMIT ".$limit) or die(mysql_error());
     $num=mysql_num_rows($query);
      if($num>0){
       $i=0;
@@ -79,7 +79,7 @@ class Product{
 
       while($i<$num)
       {
-            $prod_id=mysql_result($query,$i,"Prod_ID");
+            $prod_id=mysql_result($query,$i,"ID");
             $brand_id=mysql_result($query,$i,"Brand_ID");
             $cart_id=mysql_result($query,$i,"Cart_ID");
             $color=mysql_result($query,$i,"Color");
@@ -93,16 +93,11 @@ class Product{
             
             echo"
 
-                <div class=\"col-md-3\">
-                  <div class=\"card cust-size\">
-                    <img class=\"img_size\" src=\"".$image."\" alt=\"Norway\">
-                    <div class=\"text-center\">
-                      <h1>".$name."</h1>
-                      <p>".$color."<br>".$price."<br>".$description."</p>
-                    </div>
-                  <button  class=\"btn btn-primary waves-effect waves-light\" onclick=\"\">View More</button>
-                  </div>
-                </div>
+                <div class=\"col-md-4\">
+                 
+                   <img src=\"products/skull_clock.jpg\" class=\"img-thumbnail\" alt=\"Cinque Terre\" width=\"400\" height=\"236\">  
+                    
+                 </div>
             ";
 
       } //While closed
